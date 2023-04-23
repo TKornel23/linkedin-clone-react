047495de6a58dcf856e2d2a89dd539ef5f005551
Kornél Tóth <tkornel23@gmail.com>
2023-04-23 11:40:54 +0200

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Header from './components/header/Header';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
       
       <Header />
+
+      <div className='app__body'>
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './Sidebar.css'
+import { Avatar } from '@mui/material'
+import SidebarBackground from './assets/sidebar-background.jpg'
+
+function Sidebar() {
+
+    const recentItem = (topic) => {
+        return (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">
+                #
+            </span>
+            <p>{topic}</p>
+        </div>
+        );
+    }
+
+  return (
+    <div className='sidebar'>
+        <div className="sidebar__top">
+            <img src={ SidebarBackground } alt=''/>
+            <Avatar className='sidebar__avatar' src=''/>
+            <h2>Tóth Kornél</h2>
+            <h4>tkornel@gmail.com</h4>
+        </div>
+        <div className='sidebar__stats'>
+            <div className='sidebar__stat'>
+                <p>Who viewed you</p>
+                <p className="sidebar__statNumber">
+                    2.543
+                </p>
+            </div>
+            <div className='sidebar__stat'>
+                <p>Views on post</p>
+                <p className="sidebar__statNumber">
+                    2.448
+                </p>    
+            </div>
+        </div>
+        <div className="sidebar__bottom">
+            <p>
+                Recent
+            </p>
+            { recentItem('reactjs') }
+            { recentItem('programming') }
+            { recentItem('softwareengineering') }
+            { recentItem('design') }
+            { recentItem('developer') }
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
